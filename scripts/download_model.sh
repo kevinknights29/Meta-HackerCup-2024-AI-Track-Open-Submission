824d98db315c5879998a3f5312ded25d79cd4fb6
Kevin Knights <kevinknights29@hotmail.com>
2024-10-05 01:06:05 +0000

    feat: added repo_id and local_dir as arguments

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-# Check if python3-pip is installed already, if not, install it
-if ! command -v pip3 &> /dev/null
+# Function to display usage information
+usage() {
+    echo "Usage: $0 <repo_id> $1 <local_directory>"
+    echo "Please provide a directory path for downloading the dataset."
+    exit 1
+}
+
+# Check if exactly one argument is provided
+if [ $# -ne 2 ]; then
+    usage
+fi
+
+# Assign the provided repo_id to REPO_ID
+REPO_ID="$1"
+
+# Assign the provided directory to LOCAL_DIR
+LOCAL_DIR="$2"
+
+# Check if uv is installed already
+if ! command -v uv &> /dev/null
 then
-    echo "python3-pip is not installed. Installing..."
-    sudo apt install python3-pip -y
+    echo "Please run: . ./scripts/setup_env to have all necessary dependencies"
+    exit 1
 else
-    echo "python3-pip is already installed."
+    echo "uv is installed."
 fi
 
 # Install huggingface transfer
@@ -15,14 +33,10 @@ uv pip install huggingface_hub[hf_transfer]
 # Enable huggingface transfer
 HF_HUB_ENABLE_HF_TRANSFER=1 
 
-# Add repo id as input
-echo "Please enter the Hugging Face repository ID:"
-read repo_id
-
 # Capture token for authenticated model downloads
 echo "Please enter your Hugging Face token (input will be hidden):"
 read -s token
 
-huggingface-cli download "$repo_id" --token "$token" 
+huggingface-cli download "$REPO_ID" --token "$token" --local-dir "$LOCAL_DIR/$REPO_ID"
 
-echo "Downloaded model!"
+echo "Downloaded model to $LOCAL_DIR/$REPO_ID!"
